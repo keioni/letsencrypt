084bd2de1fc182a50a92ba97dac524052e34539b
keys <keonimar@gmail.com>
2018-08-03 03:41:37 +0900

    impl some codes

--- letsencrypt-auto.py
+++ letsencrypt-auto.py
@@ -6,11 +6,12 @@ from subprocess import Popen
 import subprocess
 
 TARGETS = list()
-OPENSSL = '/usr/bin/openssl'
-CERTBOT = '/usr/bin/certbot'
 
 
 class CertRenew:
+    OPENSSL = '/usr/bin/openssl'
+    CERTBOT = '/usr/bin/certbot'
+    # ACME_SERVER = 'https://acme-v01.api.letsencrypt.org/directory'
 
     def __init__(self, domainname: str):
         self.domainname = domainname
@@ -18,11 +19,21 @@ class CertRenew:
         self.tmpdir = mkdtemp(prefix=tmpdir_prefix)
         self.privkey = '{}/privkey.pem'.format(self.tmpdir)
         self.csr = '{}/csr.der'.format(self.tmpdir)
-        self.server = 'https://acme-v01.api.letsencrypt.org/directory'
+        self.openssl_cnf = '{}/openssl.cnf'.format(self.tmpdir)
+
+    def make_openssl_cnf(self):
+        with open(self.openssl_cnf, 'w') as f:
+            f.write('''
+[req]
+distinguished_name = dn
+[dn]
+[SAN]
+subjectAltName=DNS:{}
+            ''').format(self.domainname)
 
     def make_privkey(self):
         cmd = ' '.join([
-            OPENSSL,
+            self.OPENSSL,
             'ecparam',
             '-out {}'.format(self.privkey),
             '-name prime256v1 -genkey',
@@ -31,7 +42,7 @@ class CertRenew:
 
     def make_csr(self):
         cmd = ' '.join([
-            OPENSSL,
+            self.OPENSSL,
             'req',
             '-new',
             '-key {}'.format(self.privkey),
@@ -39,20 +50,31 @@ class CertRenew:
             '-outform der',
             '-out {}'.format(self.csr),
             '-subj "/CN={}/C=JP"'.format(self.domainname),
+            '-config {}'.format(self.openssl_cnf),
         ])
         subprocess.call(cmd, shell=True)
 
     def run_certbot(self):
         cmd = ' '.join([
-            CERTBOT,
+            self.CERTBOT,
             'certonly -t -a webroot',
             '--webroot-map {}'.format(''),
             '--redirect',
             '--csr {}'.format(self.csr),
-            '--server {}'.format(self.server),
+            # '--server {}'.format(self.server),
         ])
+        
         subprocess.call(cmd, shell=True)
 
+    def install_certs(self):
+        pass
+        
 
-for target in TARGETS:
-    CertRenew(target)
+# for target in TARGETS:
+#     CertRenew(target)
+
+le = CertRenew('keys.jp')
+print('privkey: {}').format(le.privkey)
+le.make_privkey()
+print('csr: {}').format(le.csr)
+le.make_csr()
